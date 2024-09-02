1dd81a0df8dab091b3d081c58087d018649315d9
Ketofe <92213285+Ketofe@users.noreply.github.com>
2024-09-02 09:29:54 +0200

    Add files via upload

--- pay_of/Vicsek_payof.py
+++ pay_of/Vicsek_payof.py
@@ -3,7 +3,7 @@ from scipy.spatial import cKDTree
 import scipy as sp
 from scipy import sparse
 
-#Some of the from 
+#Some of the code from
 #https://francescoturci.net/2020/06/19/minimal-vicsek-model-in-python/
 #Has been used
 
@@ -19,12 +19,13 @@ def Vicsek_payof(v_0,r, N,L,iterations,payof_matrix):
         
         #Creating the s
         initial_s=np.array([ [1,0] if k%2==0  else [0,1] for k in range(N) ])
-        s=np.array([ initial_s for i in range(iterations)])
+        s=np.zeros([iterations,N,2])
+        s[0]=initial_s
         
 
         for iteration in range(iterations-1):
                 #Updating the postions 
-                pos[iteration + 1] = (pos[iteration] + v_0 * np.stack([np.cos(theta[iteration]), np.sin(theta[iteration])])) 
+                pos[iteration + 1] = (pos[iteration] + v_0 * np.stack([np.cos(theta[iteration]), np.sin(theta[iteration])], axis=-1))
                
                 
 
@@ -32,8 +33,24 @@ def Vicsek_payof(v_0,r, N,L,iterations,payof_matrix):
                 
                 #updating the angles
                 tree = cKDTree(pos[iteration])
-                negibors=tree.query_ball_point(pos[iteration],r ) 
+                negibors_self=tree.query_ball_point(pos[iteration],r,workers=-1 ) 
+                negibors=[[ ] for k in range(N)]
+                
+                
+                #This is made using chat gpt
+                # Filter out the particle's own index from its neighbors list 
+                for particle_index in range(N):
+                          negibors[particle_index] = [i for i in negibors_self[particle_index] if i != particle_index]
+
+                
+
+                
+
                 pay_of_list=np.zeros(N)
+
+
+
+               
 
 
                 dist = tree.sparse_distance_matrix(tree, max_distance=r,output_type='coo_matrix')
@@ -44,22 +61,26 @@ def Vicsek_payof(v_0,r, N,L,iterations,payof_matrix):
                 neigh = sparse.coo_matrix((data,(dist.row,dist.col)), shape=dist.get_shape())
                 # and sum along the columns (sum over j)
                 complex_sum = np.squeeze(np.asarray(neigh.tocsr().sum(axis=1)))  
-                
-                theta[iteration+1] = np.angle(data+ complex_sum)
+                #And finally updating the angle
+                theta[iteration+1] = np.angle(complex_sum)
  
 
 
                 for particle_index in range(N):
+                      
+
+                     
+
+
+
                       negibor_indexes=negibors[particle_index]
-                      #The angles of the neigbors
-                     # angles=theta[iteration][negibor_indexes]
-                      #theta[iteration+1][particle_index]=np.angle( np.exp(theta[iteration][particle_index]*1j )+sum(np.exp(angles*1j) ) )
-                             
+                      
+                      
 
 
-                      #Making sure that there are neigbors  
+                      #Making sure that there are neigbors.  If not the payof will be 0 due np.zeros
                       if len(negibor_indexes)!=0:
-                        #Cacluting the payof   with the sum s_iAs_j  
+                        #Cacluting the payof   with the sum s_iAs_j 
                            neighbor_s=s[iteration][negibor_indexes]                                                       #The number of neighbors                                                                       
                            P=sum(   [np.dot(s[iteration][particle_index] ,np.dot(payof_matrix,k) ) for k in neighbor_s ])/len(negibor_indexes)
                            #If the length is zero the pay of will be zero due to np zeros 
@@ -69,15 +90,19 @@ def Vicsek_payof(v_0,r, N,L,iterations,payof_matrix):
                 #Now updating strategies based on neigbor with highest payof
                 for particle_index in range(N):
                      negibor_indexes=negibors[particle_index]
-                     pay_of_among_neigbors=pay_of_list[negibor_indexes]
+                    
                      #The case where max pay of among neigbors is greater than its own payof
                      if len(negibor_indexes)!=0:
+                       pay_of_among_neigbors=pay_of_list[negibor_indexes]
                        max_index = np.argmax(pay_of_among_neigbors)  # Use pay_of_among_neigbors directly
                        max_neighbor_pay_of = pay_of_among_neigbors[max_index] 
                        if max_neighbor_pay_of>pay_of_list[particle_index]:
                            s[iteration + 1][particle_index] = s[iteration][negibor_indexes[max_index]]
-                           
-                     
+                       else:
+                           s[iteration + 1][particle_index] = s[iteration][particle_index]     
+
+                     else:
+                          s[iteration + 1][particle_index] = s[iteration][particle_index]
                
         return pos,theta,s                      
 
